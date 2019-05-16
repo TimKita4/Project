a3917880f42551d2e1dc383e36b10d43e4e0af7e
FahrizalAziFerdiansyah <47998993+FahrizalAziFerdiansyah@users.noreply.github.com>
2019-05-16 12:23:14 +0700

    jbj

--- app/src/main/java/android/example/com/project/adapter/PerbaikanAdapter.java
+++ app/src/main/java/android/example/com/project/adapter/PerbaikanAdapter.java
@@ -30,14 +30,18 @@ public class PerbaikanAdapter extends RecyclerView.Adapter<PerbaikanAdapter.MyVi
     public void onBindViewHolder (MyViewHolder holder,final int position){
         holder.mTextViewId.setText("Id = " + mPerbaikanList.get(position).getId());
         holder.mTextViewNama.setText("Nama = " + mPerbaikanList.get(position).getNama());
-        holder.mTextViewNomor.setText("Nomor = " + mPerbaikanList.get(position).getNo_hp());
+        holder.mTextViewNomor.setText("No_hp = " + mPerbaikanList.get(position).getNo_hp());
+        holder.mTextViewAlamat.setText("No_hp = " + mPerbaikanList.get(position).getAlamat());
+        holder.mTextViewDeskripsi.setText("No_hp = " + mPerbaikanList.get(position).getDeskripsi());
         holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent mIntent = new Intent(view.getContext(), edit.class);
                 mIntent.putExtra("Id", mPerbaikanList.get(position).getId());
                 mIntent.putExtra("Nama", mPerbaikanList.get(position).getNama());
-                mIntent.putExtra("Nomor", mPerbaikanList.get(position).getNo_hp());
+                mIntent.putExtra("No_hp", mPerbaikanList.get(position).getNo_hp());
+                mIntent.putExtra("Alamat", mPerbaikanList.get(position).getAlamat());
+                mIntent.putExtra("Deskripsi", mPerbaikanList.get(position).getDeskripsi());
                 view.getContext().startActivity(mIntent);
             }
         });
@@ -49,13 +53,15 @@ public class PerbaikanAdapter extends RecyclerView.Adapter<PerbaikanAdapter.MyVi
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView mTextViewId, mTextViewNama, mTextViewNomor;
+        public TextView mTextViewId, mTextViewNama, mTextViewNomor,mTextViewAlamat,mTextViewDeskripsi;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             mTextViewId = (TextView) itemView.findViewById(R.id.tvId);
             mTextViewNama = (TextView) itemView.findViewById(R.id.tvNama);
             mTextViewNomor = (TextView) itemView.findViewById(R.id.tvNomor);
+            mTextViewAlamat = (TextView) itemView.findViewById(R.id.tvAlamat);
+            mTextViewDeskripsi = (TextView) itemView.findViewById(R.id.tvAlamat);
         }
     }
 }
